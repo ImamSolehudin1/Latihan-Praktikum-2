adef893cb711a791b963a2881e6323bcb46e22ee
Imam Solehudin <92722717+ImamSolehudin1@users.noreply.github.com>
2023-03-25 09:29:16 +0700

    Add files via upload

---
+++ program/variable_php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE  html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Dasar </title>
+<head>
+<body>
+    <h1> Belajar PHP Dasar </h1>
+    <h1> Menggunakan Varable </h1>
+<?php
+    echo "Hello World" ;
+    echo "<br>";
+    $nim = "312110290";
+    $nama = 'Imam Solehudin';
+    echo "NIM   : ". $nim . "<br>";
+    echo "Nama  : " . $nama;
+?>
+</body>
+</html>
